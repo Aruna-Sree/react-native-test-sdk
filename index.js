245edec59f4de31f5362740f46629f49343bac99
Aruna Kumari Yarra <aruna.yarra@broadcom.com>
2021-07-21 18:49:58 +0530

    react native

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNTestSdk } = NativeModules;
+
+export default RNTestSdk;
---
+++ ios/RNTestSdk.m
@@ -0,0 +1,13 @@
+
+#import "RNTestSdk.h"
+
+@implementation RNTestSdk
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
